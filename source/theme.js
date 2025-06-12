cdaa5d56e9b30358e21498530cfbd11904ac9ca8
Trevin Chow <trevin@bigcartel.com>
2025-06-11 22:45:47 -0700

    feat: add featured homepage video support

--- source/theme.js
+++ source/theme.js
@@ -2,6 +2,7 @@
 //= require javascripts/functions
 //= require javascripts/cart
 //= require javascripts/store
+//= require javascripts/home
 //= require javascripts/product-payment-messaging
 //= require javascripts/product-option-groups
 //= require javascripts/product-carousel
